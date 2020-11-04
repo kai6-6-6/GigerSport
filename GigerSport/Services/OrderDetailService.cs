35c3b1ae3b90f63aa426d36c81b98d831cab665c
登凱 鍾 <lovemark2413@gmail.com>
2020-11-04 20:06:37 +0800

    service

--- GigerSport/Services/OrderDetailService.cs
+++ GigerSport/Services/OrderDetailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GigerSport.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +8,13 @@ namespace GigerSport.Services
 {
     public class OrderDetailService
     {
-        public 
+        public orderDetailModel GetOrderDetail()
+        {
+
+
+
+            return null;
+        }
 
     }
 }
