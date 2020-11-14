0a205ab942f85e0a0c7e6d22816e9d02fc846989
登凱 鍾 <lovemark2413@gmail.com>
2020-11-14 20:22:09 +0800

    export form

---
+++ GigerSport/Controllers/ExcelController.cs
@@ -0,0 +1,19 @@
+﻿using GigerSport.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigerSport.Controllers
+{
+    public class ExcelController : Controller
+    {
+        public ActionResult ForCustomer()
+        {
+            FormForCustomerService MakeForm = new FormForCustomerService();
+            MakeForm.MakeFormForCustomer();
+            return RedirectToAction("CreateOrder", "Order");
+        }
+    }
+}
